6951549b0833fc39905a74c50fe9781a3f4df99d
sapae <57509402+sapae@users.noreply.github.com>
2020-04-22 19:30:41 +0000

    First commit

---
+++ game.py
@@ -0,0 +1,41 @@
+import time
+import random 
+import sys
+
+#sys : This module provides access to some variables used or maintained by the interpreter and to functions that interact strongly with the interpreter. It is always available.
+
+booli = True
+inp = "Ask the Magic 8-Ball a question (Return or enter to quit) "
+
+while 1:
+    question = input(inp)
+    answer = random.randint(1,8)
+    time.sleep(2)
+
+    if question == "":
+        sys.exit()
+    elif answer == 1:
+        print("Maybe Yes")
+        break
+    elif answer == 2:
+        print("Absolutely No")
+        break
+    elif answer == 3:
+        print("Ask again later, I'm not so sure about it")
+        break
+    elif answer == 4:
+        print("How dare you Ask, you know deep in your heart that it is true")
+        break
+    elif answer == 5:
+        print("Not really .. sorry!")
+        break
+    elif answer == 6:
+        print("Things may change in your life if you knew the answer, are you sure you wanna know ?! Ask later")
+        break
+    elif answer == 7:
+        print("I say NO")
+        break
+    elif answer == 8:
+        print("No way! duuude!")
+        break
+
